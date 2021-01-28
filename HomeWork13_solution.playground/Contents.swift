dbf52ee6c99d2e6df4680bc9bd2c59ed1b6d44a4
NastyaS <nastya.soiko.97@gmail.com>
2021-01-28 13:43:40 +0300

    add solution for third task and part of fourth task

--- HomeWork13_solution.playground/Contents.swift
+++ HomeWork13_solution.playground/Contents.swift
@@ -269,14 +269,83 @@ buildArrayCatType(cats: cats)
 //    Если в списке попался Танцор, то вывести его зарплату за день, если Музыкант, то отобразить любимый инструмент.
 
 //    ===============================================
+//                      Task 3
+//    ===============================================
+//
+//    1. Создать протокол BusinessTrip:
+//    - статическая переменная - доступные страны. на чтение и на запись
+//    - страна назначения. только на чтение
+//    - продолжительность поездки. только на чтение
+//    - флаг isCurrentlyInBusinessTrip. На чтение и на запись
+//    - методы startTrip() endTrip()
+//
+//    2. Подчиниться протоколу BusinessTrip в классе Художник. Реализовать свойства и методы:
+//    - художнику доступны для командировки Греция, Польша, Беларусь, Германия и Америка
+//    - добавить инициализатор класса художник, который принимает страну назначения и продолжительность поездки
+//    - добавить пустой инициализатор класса художник, который устанавливает рандомную страну назначения и рандомную продолжительнсть поездки.
+//    - реализовать функцию начала поездки, которая начинают поездку в страну назначения только если она есть в списке доступных стран
+//    - реализовать функцию конца поездки, которая выводит информацию о поездке: страна назначения, продолжительность.
+//
+//    3. Создать протокол PayableBusinessTrip, наследующийся от протокола BusinessTrip:
+//    - salaryInBusinessTrip. Только на чтение
+//    - salaryCoefficient. Только на чтение
+//    - метод paySalary()
+//
+//    4. Подчиниться в классе Музыкант протоколу PayableBusinessTrip:
+//    - salaryCoefficient зависит от стажа работы: 1-2 года - 1x, 3-5 лет - 1.5x, > 5 лет -  2x.
+//    - salaryInBusinessTrip вычисляется на основе зарплаты за день (если артист отказался ее называть, то зарплата по умолчанию - 80), помноженной на salaryCoefficient
+//    - paySalary() - выводит в консоль сумму, которую музыкант заработал за время командировки.
+//    - музыкантам доступны для командировки Латвия, Литва, Польша, Германия и Италия.
+//    - музыканты хотят работать только в Германии и Италии, поэтому отказываются ехать в другие страны и, соответственно, функция начала поездки не отработает.
+//
+//    5. Подчините класс Танцора протоколу PayableBusinessTrip:
+//    - salaryInBusinessTrip вычисляется по такой же логике, как и у музыканта, только потом увеличивается ещё на на 10%
+//    - startTrip() - танцор не отправляется в командировку, если его зарплата за день в командировке меньше 100
+//    - paySalary() - выводит в консоль сумму, которую музыкант заработал за время командировки.
+//    - танцорам доступны для командировки Украина, Грузия, Швеция, Швейцария и Финляндия.
+//
+//    ===============================================
 //                       Solution
 //    ===============================================
 
+enum Country: CaseIterable {
+    case belarus, russia, ukraine, poland, turkey, germany, america, greace
+}
 
-class Artist {
-    enum Country: CaseIterable{
-        case belarus, russia, ukraine, poland, turkey
+protocol ProtocolForArtist {
+    var name: String { get }
+}
+
+protocol BusinessTrip {
+    var freeCountry: Country { get set }
+    var destinationCountry: Country { get }
+    var durationOfTrip: Int { get }
+    var isCurrentlyInBusinessTrip: Bool { get set }
+    
+    func startTrip()
+    func endTrip()
+}
+
+protocol PayableBusinessTrip: BusinessTrip {
+    var salaryInBusinessTrip: Int { get }
+    var salaryCoefficient: Int { get }
+    
+    func paySalary()
+}
+
+class Artist: ProtocolForArtist {
+    var name: String
+    var countryJob: Country
+    var weekSalary: Int = 300
+    
+    var procentOfWorkExperienceOfArtist: Int { 100 * workExperience / age }
+    
+    class var averageSalary: Int {
+        return 1200
     }
+    
+    var daySalary:Int { self.weekSalary / 5 }
+    private var _daySalary: Int = 70
     
     var age: Int {
         get {
@@ -288,8 +357,8 @@ class Artist {
             }
         }
     }
-    var countryJob: Country
-    var weekSalary: Int = 300
+    private var _age: Int = 45
+    
     var workExperience: Int {
         get {
             return self._workExperience
@@ -300,38 +369,29 @@ class Artist {
             }
         }
     }
-    var daySalary:Int { self.weekSalary / 5 }
-    }
-    var procentOfWorkExperienceOfArtist: Int { 100 * self.workExperience / self.age }
-    
-    class var averageSalary: Int {
-        return 1200
-    }
-    private var _age: Int = 23
-    private(set) var name: String = ""
     private var _workExperience: Int = 4
-    private var _daySalary: Int = 70
+
     
+    init(name: String, workExperience: Int, weekSalary: Int) {
+        self.name = ""
+        self.workExperience = workExperience
+        self.weekSalary = weekSalary
+    }
     
-    init(name: String, countryJob: Country, workExperience: Int) {
-        self.name = name
+    init(name: String, weekSalary: Int, countryJob: Country, workExperience: Int) {
+        self.name = ""
+        self.weekSalary = weekSalary
         self.countryJob = countryJob
         self.workExperience = workExperience
     }
     
-    init(name: String, weekSalary: Int, countryJob: Country) {
-        self.name = name
-        self.weekSalary = weekSalary
-        self.countryJob = countryJob
-    }
-    
-    static func allCountries() -> [Country] {
-        var fromEnunOfCoytries = Artist.Country.AllCases
-            .map({ "\($0)" })
-            .joined(separator: ", ")
+    class func allCountries() -> [Country] {
+        var fromEnunOfCoytries: [Country] {
+            return [.belarus, .russia, .ukraine, .poland, .turkey, .germany, .america, .greace]
+        }
         var arrayOfAllCountries: [Country] = []
         for _ in fromEnunOfCoytries {
-            arrayOfAllCountries.append(fromEnunOfCoytries)
+            arrayOfAllCountries.append(contentsOf: fromEnunOfCoytries)
         }
         return arrayOfAllCountries
     }
@@ -342,85 +402,162 @@ class Artist {
     
     private func infoAboutArtist() {
         print("""
-            The name is \(self.name) and \(self.age)
-            His experience is \(self.workExperience)
-            From country \(self.countryJob)
-            His salary is \(self.daySalary)
-            """)
+        The name is \(self.name) and \(self.age)
+        His experience is \(self.workExperience)
+        From country \(self.countryJob)
+        His salary is \(self.daySalary)
+        """)
     }
     
     func perfomOnStage() {
         print("""
-            The name is \(self.name) and \(self.age)
-            His experience is \(self.workExperience)
-            From country \(self.countryJob)
-            His salary is \(self.daySalary)
-            выступает в жанре поп и исполняет знаменитую песню
-            """)
-    }
-    
-    class Musician: Artist {
-        var favoriteInstrument: String
-        
-        init(name: String, countryJob: Country, workExperience: Int, favoriteInstrument: String) {
-            self.favoriteInstrument = favoriteInstrument
-            super.init(name: name, countryJob: countryJob, workExperience: workExperience)
-        }
-    }
-        override class func printInfo() {
-            print("Название класса - Musician")
-            
-        }
-        
-        
-    class Paintist: Artist {
-        override var name: String {
-            if super.name = name {
-                print("Художник не хочет использовать своё родное имя")
-            }
-            return super.name.shuffled()
-        }
-            
-            override class func printInfo() {
-                print("Название класса - Paintist.")
-            }
-        }
-    class Dancer: Artist {
-        override var daySalary: Int {
-            if super.daySalary < 100 {
-                print("Танцор отказывается выступать")
-            }
-            return super.daySalary
-        }
-        override class func printInfo() {
-            print("Название класса - Dancer.")
-        }
+        The name is \(self.name) and \(self.age)
+        His experience is \(self.workExperience)
+        From country \(self.countryJob)
+        His salary is \(self.daySalary)
+        выступает в жанре поп и исполняет знаменитую песню
+        """)
     }
 }
 
-Художник не хочет использовать своё родное имя, заменяя его на сценическое, которое состоит из перемешанных букв родного имени.
+class Musician: Artist {
+    var favoriteInstrument: String
+    
+    init(name: String, weekSalary: Int, countryJob: Country, workExperience: Int, favoriteInstrument: String) {
+        self.favoriteInstrument = favoriteInstrument
+        super.init(name: name, weekSalary: weekSalary, countryJob: countryJob, workExperience: workExperience)
+    }
+    
+    override class func printInfo() {
+        print("Название класса - Musician")
+    }
+}
 
-let firstArgumentArtist = Artist.init(name: "Petr", countryJob: .belarus, workExperience: 14)
-let secondArgumentArtist = Artist.init(name: "Vasya", weekSalary: 650, countryJob: .poland)
-let firstArgumentMusician = Artist.Musician.init(name: "Alesya", countryJob: .poland, workExperience: 16, favoriteInstrument: "guitar")
-let secondArgumentMusician = Artist.Musician.init(name: "Alex", countryJob: .turkey, workExperience: 17, favoriteInstrument: "piano")
-let firstArgumentPaintist = Artist.Paintist.init(name: "Sam", weekSalary: 400, countryJob: .belarus)
-let secondArgumentPaintist = Artist.Paintist.init(name: "Tanya", weekSalary: 100, countryJob: .russia)
-let firstArgumentDancer = Artist.Dancer.init(name: "Valya", weekSalary: 340, countryJob: .ukraine)
-let secondArgumentDancer = Artist.Dancer.init(name: "Vlad", countryJob: .ukraine, workExperience: 14)
+
+class Paintist: Artist, BusinessTrip {
+    var freeCountry: Country
+    var destinationCountry: Country
+    var durationOfTrip: Int
+    var isCurrentlyInBusinessTrip: Bool
+    
+    override var name: String {
+        String(super.name.shuffled())
+    }
+    
+    func freeCountryForPaintist() -> (Country, Country, Country, Country, Country) {
+        return (.greace, .poland, .belarus, .germany, .america)
+    }
+    
+    func startTrip() {
+    }
+    
+    func endTrip() {
+        print("""
+            страна назначения \(self.destinationCountry)
+            продолжительность \(self.durationOfTrip)
+            """)
+    }
+    
+    override class func printInfo() {
+        print("Название класса - Paintist.")
+    }
+    
+    init(destinationCountry: Country, durationOfTrip: Int) {
+        self.destinationCountry = destinationCountry
+        self.durationOfTrip = durationOfTrip
+    }
+    init() {
+        self.destinationCountry = Country.allCases.randomElement()!
+        self.durationOfTrip = Int.random(in: 0...100000)
+    }
+    
+    override init(name: String, weekSalary: Int, countryJob: Country, workExperience: Int){
+        super.init(name: name, weekSalary: weekSalary, countryJob: countryJob, workExperience: workExperience)
+    }
+}
+
+class Dancer: Artist {
+    override var daySalary: Int {
+        if super.daySalary < 100 {
+            print("Танцор отказывается выступать")
+        }
+        return super.daySalary
+    }
+    override class func printInfo() {
+        print("Название класса - Dancer.")
+    }
+}
+
+
+let firstArgumentArtist = Artist.init(name: "Petr", weekSalary: 450, countryJob: .belarus, workExperience: 14)
+let secondArgumentArtist = Artist.init(name: "Vasya", weekSalary: 650, countryJob: .poland, workExperience: 30)
+let firstArgumentMusician = Musician.init(name: "Alesya", weekSalary: 800, countryJob: .poland, workExperience: 35, favoriteInstrument: "guitar")
+let secondArgumentMusician = Musician.init(name: "Alex", weekSalary: 90, countryJob: .turkey, workExperience: 17, favoriteInstrument: "piano")
+let firstArgumentPaintist = Paintist.init(name: "Sam", weekSalary: 400, countryJob: .belarus, workExperience: 10)
+let secondArgumentPaintist = Paintist.init(name: "Tanya", weekSalary: 100, countryJob: .russia, workExperience: 23)
+let firstArgumentDancer = Dancer.init(name: "Valya", weekSalary: 340, countryJob: .ukraine, workExperience: 5)
+let secondArgumentDancer = Dancer.init(name: "Vlad", weekSalary: 123, countryJob: .ukraine, workExperience: 14)
 let sumOfArray: [Artist] = [firstArgumentArtist, secondArgumentArtist, firstArgumentMusician, secondArgumentMusician, firstArgumentPaintist, secondArgumentPaintist, firstArgumentDancer, secondArgumentDancer]
 
 func buildArrayArtistType(artist: [Artist]) -> [Artist] {
     for artist in sumOfArray {
         if artist is Dancer {
             print("\(artist.weekSalary)")
-        } else if artist is Musician {
+        } else if let artist = artist as? Musician {
             print("\(artist.favoriteInstrument)")
         } else {
-            artist.perfomOnStage
+            artist.perfomOnStage()
         }
     }
     return sumOfArray
 }
 
 buildArrayArtistType(artist: sumOfArray)
+
+
+//    ===============================================
+//                     Task 4
+//    ===============================================
+//
+//    1. Создайте функцию, которая принимает 2 объекта и сравнивает их с помощью замыкания:
+//    - 1 аргумент - типа Артист
+//    - 2 аргумент - типа Артист
+//    - 3 аргумент - замыкание, которое принимает Артистов (1 и 2 аргумент из функции) и возвращает Bool - равны они или нет
+//    - Результат сравнения выведите в консоль
+//    2. Создайте функцию, которая принимает 2 артистов и возвращает Bool. Если у артистов одинаковый стаж работы и зарплата в неделю, то они равны.
+//    Вызовите функцию из 1 пункта, передав ей в качестве 3 аргумента только что созданную функцию.
+//    3. Создайте замыкание, которое принмиает 2 артистов и возвращает Bool. Если один артист музыкант, а второй художник и у них одинаковый стаж работы, то они равны.
+//    Вызовите функцию из 1 пункта, передав ей в качестве 3 аргумента только что созданное замыкание.
+//    4. Вызовите функцию из 1 пункта, реализовав замыкание сразу же. Если у Артистов одинаковое имя, то они равны.
+//    Не сокращайте имена аргументов в замыкании.
+//    5. Вызовите функцию из 1 пункта, реализовав замыкание сразу же. Если это Художник и Танцор, то они равны.
+//    Воспользуйтесь $0 и $1 при обращении к аргументам замыканиия.
+//    6. Вызовите функцию из 1 пункта, реализовав замыкание сразу же. Если это Художник и Музыкант, то они будут равны при равенстве имени, возраста и стажа работы.
+//    Максимально сократите тело замыкания, а также сделайте его в виде trailing closure.
+
+
+func comparisonOfArtists(firstArgument: [Artist], _ secondArgument: [Artist]) -> Bool {
+    func compare<T: Equatable>(firstArgument: Artist, _ secondArgument: Artist, t: T.Type) -> Bool {
+        return firstArgument as? T == secondArgument as? T
+    }
+    guard firstArgument.count ==  secondArgument.coun else { return false }
+    return firstArgument.indices.reduce(true) {
+        guard let _firstArgument = firstArgument[$1], let _secondArgument = secondArgument[$1] else { return $0 && _firstArgument[$1] == nil && secondArgument[$1] == nil }
+        
+        switch $1 {
+        case 0:
+            return $0 && compare(_firstArgument, _secondArgument, t: String.self)
+        case 1:
+            return $0 && compare(_firstArgument, _secondArgument, t: String.self)
+        default:
+            return false
+        }
+    }
+}
+
+func jobExpiriense(firstArgument: [Artist], _secondArgument: [Artist]) -> Bool{
+    guard firstArgument.workExperience == _secondArgument.workExperience ||  firstArgument.weekSalary == _secondArgument.weekSalary else { return false }
+}
+
+compare<T(firstArgument: sumOfArray.firstArgumentArtist, _ secondArgument: sumOfArray.secondArgumentMusician, t: jobExpiriense(firstArgument: sumOfArray.secondArgumentDancer, _secondArgument: sumOfArray.firstArgumentPaintist))
+
